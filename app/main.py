22af97d1788093f6c84f24902cf776e13fc8a7d4
alexanderjguzy <alexanderjguzy@gmail.com>
2026-05-04 18:41:00 -0500

    added login feature and security to project

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .database import engine, Base
 from . import models
 from .routes import user
 from .routes import file
+from .routes import auth
 
 app = FastAPI()
 
@@ -11,6 +12,8 @@ Base.metadata.create_all(bind=engine)
 app.include_router(user.router)
 
 app.include_router(file.router)
+
+app.include_router(auth.router)
 
 @app.get("/")
 def read_root():
